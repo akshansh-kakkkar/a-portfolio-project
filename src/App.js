4100456ed814abd475db1baba62737856ae4b44d
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-02-04 14:48:07 +0530

    Clean up App.js by removing unused imports and variables
    Removed WelcomeMessage import and student details.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import PortfolioCard from './Components/PortfolioCard';
-import WelcomeMessage from './WelcomeMessage'
 function App() {
-  const studentName = "Alice";
-  const favoriteColor = "Blue";
-  const age = 25;
   return (
     <div className='App'>
       <PortfolioCard/>
