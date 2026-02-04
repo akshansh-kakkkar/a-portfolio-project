8b5d736849dd68f3e4fe338a7949d610af924779
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-02-04 14:42:14 +0530

    Delete WelcomeMessage component
    Remove WelcomeMessage component from the project.

--- src/WelcomeMessage.js
+++ src/WelcomeMessage.js
@@ -1,9 +1,1 @@
-function WelcomeMessage(){
-    return(
-        <div>
-            <h2>Congratulations!</h2>
-            <p>You jus created your first React file</p>
-        </div>
-    )
-}
-export default WelcomeMessage
+
